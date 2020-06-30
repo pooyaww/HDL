664f72ee8421e24e61a26de48baabd5bbd268d0a
Puya Amiri <puya.amiri@dfki.de>
2020-06-30 17:23:39 +0200

    shift reg via variable

--- shift_register/VHDL/shift_register.vhd
+++ shift_register/VHDL/shift_register.vhd
@@ -11,27 +11,49 @@ entity shift_register is
 end shift_register;
 
 -- Each register is declared as a signal variable
-architecture rtl_1 of shift_register is
+architecture rtl_signal of shift_register is
 
     signal r0_data : std_logic_vector(1 downto 0);
     signal r1_data : std_logic_vector(1 downto 0);
     signal r2_data : std_logic_vector(1 downto 0);
     signal r3_data : std_logic_vector(1 downto 0);
 
-begin
-    o_data <= r3_data;
-    p_sreg : process(i_clk, i_rstb)
-             begin
-                 if (i_rstb = '0') then
-                    r0_data <= (others => '0');
-                    r1_data <= (others => '0');
-                    r2_data <= (others => '0');
-                    r3_data <= (others => '0');
-                 elsif (rising_edge(i_clk)) then
-                    r0_data <= i_data;
-                    r1_data <= r0_data;
-                    r2_data <= r1_data;
-                    r3_data <= r2_data;
-                 end if;
-             end process p_sreg;
-end rtl_1;
+    begin
+        o_data <= r3_data;
+        p_sreg : process(i_clk, i_rstb)
+                begin
+                    if (i_rstb = '0') then
+                        r0_data <= (others => '0');
+                        r1_data <= (others => '0');
+                        r2_data <= (others => '0');
+                        r3_data <= (others => '0');
+                    elsif (rising_edge(i_clk)) then
+                        -- signals assigment order is not important
+                        r0_data <= i_data;
+                        r1_data <= r0_data;
+                        r2_data <= r1_data;
+                        r3_data <= r2_data;
+                    end if;
+                end process p_sreg;
+    end rtl_signal;
+
+architecture rtl_variable of shift_register is
+    begin
+        p_sreg : process(i_clk, i_rstb)
+        variable v_0_data : std_logic_vector(1 downto 0);
+        variable v_1_data : std_logic_vector(1 downto 0);
+        variable v_2_data : std_logic_vector(1 downto 0);
+        begin
+            if (i_rstb = '0') then
+                v_0_data := (others => '0');
+                v_1_data := (others => '0');
+                v_2_data := (others => '0');
+                o_data   <= (others => '0');
+            elsif(rising_edge(i_clk)) then
+                o_data    <= v_2_data;
+                v_2_data  := v_1_data;
+                v_1__data := v_0_data;
+                v_0_data  := i_data;
+            end if;
+        end process p_sreg;
+    end rtl_variable;
