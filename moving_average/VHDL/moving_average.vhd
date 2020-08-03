93de9ab372d5649366107ce5869186cc05277323
Puya Amiri <puya.amiri@dfki.de>
2020-08-03 15:50:52 +0200

    moving average wip

--- moving_average/VHDL/moving_average.vhd
+++ moving_average/VHDL/moving_average.vhd
@@ -18,9 +18,36 @@ entity moving_average is
         i_data_ena   : in std_logic;
         i_data_ena   : in std_logic_vector (G_NBIT-1 downto 0);
         -- output
-        o_data_valid : out std_logic_vector;
+        o_data_valid : out std_logic;
         o_data       : out std_logic_vector (G_NBIT-1 downto 0);
 end moving_average;
 
 architecture behavioral of moving_average is
     type t_moving_average is array (0 to 2**G_AVG_LEN_LOG) of std_logic_vector (G_NBIT downto 0);
+    signal p_moving_average : t_moving_average;
+    signal r_acc            : signed(G_NBIT+G_AVG_LEN_LOG-1 downto 0); -- average accumulator
+    signal r_data_valid     : std_logic;
+
+begin
+    p_average : process(i_clk, i_rstb)
+    begin
+        -- all registers and output signals are assigned to zero
+        if (i_rstb = '0') then
+            r_acc            <= (others => '0');
+            p_moving_average <= (others => (others => '0'));
+            r_data_valid     <= '0';
+            o_data_valid     <= '0';
+            o_data           <= (others => '0');
+        elsif (rising_edge(i_clk)) then
+            r_data_valid <= i_data_ena;
+            o_data_valid <= r_data_valid;
+            if (i_sync_reset = '1') then
+                r_acc <= (others => '0');
+                p_moving_average <= (others => (others => '0'));
+            elsif (i_data_ena = '1') then
+                -- main computation
+            end if;
+            -- divide by 2^G_AVG_LEN_LOG
+        end if;
+    end process p_average;
+end behavioral;
