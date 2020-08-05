3676575bbe0b937b415a79f35cf6d58176a1372f
Puya Amiri <puya.amiri@dfki.de>
2020-08-05 13:08:29 +0200

    moving avg

--- moving_average/VHDL/moving_average.vhd
+++ moving_average/VHDL/moving_average.vhd
@@ -16,7 +16,7 @@ entity moving_average is
         i_sync_reset : in std_logic;
         -- input
         i_data_ena   : in std_logic;
-        i_data_ena   : in std_logic_vector (G_NBIT-1 downto 0);
+        i_data       : in std_logic_vector (G_NBIT-1 downto 0);
         -- output
         o_data_valid : out std_logic;
         o_data       : out std_logic_vector (G_NBIT-1 downto 0);
@@ -45,9 +45,13 @@ begin
                 r_acc <= (others => '0');
                 p_moving_average <= (others => (others => '0'));
             elsif (i_data_ena = '1') then
+                --shift
+                p_moving_average <= signed(i_data)&p_moving_average(0 to p_moving_average'length-2);
                 -- main computation
+                r_acc <= r_acc + signed(i_data) - p_moving_average(p_moving_average'length-1);
             end if;
-            -- divide by 2^G_AVG_LEN_LOG
+            -- divide by 2^G_AVG_LEN_LOG (removing G_AVG_LEN_LOG bit from LSB)
+            o_data <= std_logic_vector(r_acc(G_NBIT + G_AVG_LEN_LOG - 1 downto G_AVG_LEN_LOG));
         end if;
     end process p_average;
 end behavioral;
